a8d56bee28c76c8c802cf0d6266c735e1dbfbc46
Jeremy Granadillo <akakitsu@gmail.com>
2013-10-22 16:03:09 +0200

    Convert function comments to JSDoc

--- js/script.js
+++ js/script.js
@@ -38,9 +38,10 @@ var SwipeTable = (function(){
 
     //=== Functions ===
 
-    // init()
-    //  Fetches the viewport size and sets the
-    //  pageSize(num of rows) based on rowHeight
+    /**
+     * Fetch the viewport size and set the
+     * pageSize(num of rows) based on rowHeight
+     */
     var init = function(){
         // responsejs.com/labs/dimensions/
         var matchMedia = window.matchMedia || window.msMatchMedia;
@@ -58,18 +59,21 @@ var SwipeTable = (function(){
         pageSize = Math.floor(viewportH / rowHeight);
     }
 
-    // makeRequest(queries)
-    //  Takes an object that contains:
-    //REQ* server: address of the API
-    //REQ* pageSize
-    //REQ* table: container to attach the results to
-    //   * page
-    //   * timestamp
-    //   * sortField
-    //   * sortAsc
-    //  Items marked with REQ are required or the request will fail.
-    //  Fetches according to parameters given
-    //   and gives results + table to parseResponse.
+    /**
+     * Takes an object that contains:
+     * REQ* server: address of the API
+     * REQ* pageSize
+     * REQ* table: container to attach the results to
+     *    * page
+     *    * timestamp
+     *    * sortField
+     *    * sortAsc
+     * Items marked with REQ are required or the request will fail.
+     *
+     * Fetches according to parameters given
+     * and gives results + table to parseResponse.
+     * @param {Object}queries Object containing quieries
+     */
     var makeRequest = function (queries){
         if(!queries.server){
             console.log("No server provided to makeRequest, not making request");
@@ -147,10 +151,13 @@ var SwipeTable = (function(){
         fillTable(table, JSON.parse(response));
     };
 
-    // createTable()
-    //  Creates a table with appropriate headers(thead).
-    //  Increments totalTables.
-    //  Returns a partial table.
+    /**
+     * Creates a table with appropriate headers(thead).
+     *
+     * Increments totalTables.
+     *
+     * Returns a partial table.
+     */
     var createTable = (function(){
         console.log("Creating Table.");
         var tableInstance;
@@ -185,11 +192,15 @@ var SwipeTable = (function(){
         };
     }());
 
-    // fillTable(table, dataSet)
-    //  Goes through dataSet and attaches rows to
-    //   the table reference, then attaches the
-    //   table to the container in the DOM.
-    //  Calls tableDone() when done.
+    /**
+     * Goes through dataSet and attaches rows to
+     * the table reference, then attaches the
+     * table to the container in the DOM.
+     *
+     * Calls tableDone() when done.
+     * @param table Partial table to fill
+     * @param dataSet Parsed data to fill with
+     */
     var fillTable = function(table, dataSet){
         console.log("Filling table.");
 
@@ -239,14 +250,16 @@ var SwipeTable = (function(){
         tableDone();
     };
 
-    // tableDone()
-    //  Increments doneTables and executes if equal to totalTables.
-    //  Checks if mySwipe is made and if it isn't,
-    //   makes it and gives arguments to mySwipe.
-    //  If it's the first table to be finished,
-    //   sets up the next one to be fetched and placed,
-    //   also updates the header.
-    //  If mySwipe was already made, calls mySwipe.setup.
+    /**
+     *  Increments doneTables and executes if equal to totalTables.
+     *  Checks if mySwipe is made and if it isn't,
+     *  makes it and gives arguments to mySwipe.
+     *  If it's the first table to be finished,
+     *  sets up the next one to be fetched and placed,
+     *  also updates the header.
+     *
+     *  If mySwipe was already made, calls mySwipe.setup.
+     */
     var tableDone = function(){
         doneTables += 1;
         if (doneTables === totalTables){
@@ -274,9 +287,11 @@ var SwipeTable = (function(){
         }
     };
 
-    // nextPage()
-    //  Gets position from mySwipe.
-    //  Calls createTable(), passes it to makeRequest
+    /**
+     * Gets position from mySwipe.
+     *
+     * Calls createTable(), passes it to makeRequest.
+     */
     var nextPage = function(){
         var pos = window.mySwipe.getPos() + 1;
 
@@ -300,10 +315,12 @@ var SwipeTable = (function(){
         }
     };
 
-    // updateHeader(element)
-    //  Copy the given element,
-    //  Paste it in the header container and attach onscroll listener,
-    //  Execute updateScroll calls when appropriate to update positions.
+    /**
+     * Copy the given element,
+     * paste it in the header container and attach onscroll listener,
+     * execute updateScroll calls when appropriate to update positions.
+     * @param element
+     */
     var updateHeader = function(element){
         var copy = element.cloneNode(true);
         var header = document.getElementsByClassName('swipe-table-header')[0];
@@ -331,9 +348,10 @@ var SwipeTable = (function(){
         }
     };
 
-    // updateScroll()
-    //  Exposes scroll positions to the outside
-    //   and function to update all scrollables.
+    /**
+     * Exposes scroll positions to the outside
+     * and function to update all scrollables.
+     */
     var updateScroll = (function(){
         var position;
         console.log("Entered updateScroll");
